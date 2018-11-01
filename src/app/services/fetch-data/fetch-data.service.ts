2e642c962f52124852f9d4cbd1904d5820558965
KiraBurova <coca-cola-from-hell@yandex.ru>
2018-11-01 09:56:03 +0300

    last fixes to styles and structure

--- src/app/services/fetch-data/fetch-data.service.ts
+++ src/app/services/fetch-data/fetch-data.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 
 import { Observable } from "rxjs";
 
-import { API_KEY } from "../../../environments/environment";
+import { LAST_FM_API_KEY } from "../../../config/config";
 
 @Injectable({
   providedIn: "root"
@@ -13,13 +13,13 @@ export class FetchDataService {
 
   getArtists(): Observable<any> {
     return this.http.get<{}>(
-      `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=italy&limit=10&api_key=${API_KEY}&format=json`
+      `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=italy&limit=10&api_key=${LAST_FM_API_KEY}&format=json`
     );
   }
 
   getTracks(artist): Observable<any> {
     return this.http.get<{}>(
-      `http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=${artist}&limit=10&api_key=${API_KEY}&format=json`
+      `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&mbid=${artist}&limit=10&api_key=${LAST_FM_API_KEY}&format=json`
     );
   }
 }
